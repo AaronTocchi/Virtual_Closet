05acd3732d2d7a9bec0ab723a6928802c6e9257e
Chuhan Jie <jiechuhan1996@gmail.com>
2019-10-30 12:52:32 -0700

    new api

--- Project-Two-Starter/db/schema.sql
+++ Project-Two-Starter/db/schema.sql
@@ -1,2 +1,13 @@
 DROP DATABASE IF EXISTS closet_db;
 CREATE DATABASE closet_db;
+
+use closet_db;
+
+INSERT INTO 
+	Closets (name, type, color, temp, waterProof) 
+VALUES 
+	('vans', "shoe", "white", "warm", false),
+    ('jordans', "top", "white", "warm", false),
+    ('keds', "shoe", "white", "warm", false),
+    ('adidas', "bottom", "white", "warm", false),
+    ('nikes', "accessory", "white", "warm", false);
